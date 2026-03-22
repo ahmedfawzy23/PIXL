25f09de11aa54fef4643b9b4f48bd07cb7bdf521
Ahmed Fawzy <ahmedfawzy169236@gmail.com>
2026-03-22 13:05:31 +0200

    feat: Introduce initial UI for feed, profile, and common layout components.

--- app/View/Components/Layout.php
+++ app/View/Components/Layout.php
@@ -11,10 +11,7 @@ class Layout extends Component
     /**
      * Create a new component instance.
      */
-    public function __construct()
-    {
-        //
-    }
+    public function __construct(public string $title = "PIXL") {}
 
     /**
      * Get the view / contents that represent the component.
